830f1c3494290d0ad7a4a08c9e8d296f44473027
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2021-02-19 17:27:13 +0330

    Add url for new views

--- my_site/urls.py
+++ my_site/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home_page
+from .views import home_page, about_page, contact_page, features, preview
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page),
+    path('about-us', about_page),
+    path('contact-us', contact_page),
+    path('preview', preview),
+    path('features', features),
 ]
