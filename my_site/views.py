f11a715749023433f94b8846e8f94604828eee2f
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2021-02-19 17:25:05 +0330

    Add multiple views

--- my_site/views.py
+++ my_site/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 
 def home_page(request):
     return HttpResponse("<h1>Hello World</h1>")
+
+
+def about_page(request):
+    return HttpResponse("<h1>About Us</h1>")
+
+
+def contact_page(request):
+    return HttpResponse("<h1>Contact Us</h1>")
+
+
+def features(request):
+    return HttpResponse("<h1>Features</h1>")
+
+
+def preview(request):
+    return HttpResponse("<h1>Preview</h1>")
