be979488d84774328c82d9e4f6b81ee362efcb09
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2021-02-19 19:59:35 +0330

    extend base.html file

--- my_site/views.py
+++ my_site/views.py
@@ -4,29 +4,24 @@ from django.shortcuts import render
 
 def home_page(request):
     the_title = 'Home'
-    the_header = 'Hello, world!!!'
-    return render(request, "Home/home.html", {'title': the_title, 'header': the_header})
+    return render(request, "Home/home.html", {'title': the_title})
 
 
 def about_page(request):
     the_title = 'About Us'
-    the_header = 'About Us!!!'
-    return render(request, "Home/home.html", {'title': the_title, 'header': the_header})
+    return render(request, "Home/about.html", {'title': the_title})
 
 
 def contact_page(request):
     the_title = 'Contact Us'
-    the_header = 'Contact Us!!!'
-    return render(request, "Home/home.html", {'title': the_title, 'header': the_header})
+    return render(request, "Home/contact.html", {'title': the_title})
 
 
 def features(request):
     the_title = 'Features'
-    the_header = 'Features!!!'
-    return render(request, "Home/home.html", {'title': the_title, 'header': the_header})
+    return render(request, "Home/feature.html", {'title': the_title})
 
 
 def preview(request):
     the_title = 'Preview'
-    the_header = 'Preview!!!'
-    return render(request, "Home/home.html", {'title': the_title, 'header': the_header})
+    return render(request, "Home/preview.html", {'title': the_title})
