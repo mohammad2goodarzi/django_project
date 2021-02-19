e7fe78a2b7a785067c4233548b414bf7b37b525d
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2021-02-19 19:18:25 +0330

    dynamic html file:)

--- my_site/views.py
+++ my_site/views.py
@@ -3,20 +3,30 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, "Home/home.html")
+    the_title = 'Home'
+    the_header = 'Hello, world!!!'
+    return render(request, "Home/home.html", {'title': the_title, 'header': the_header})
 
 
 def about_page(request):
-    return HttpResponse("<h1>About Us</h1>")
+    the_title = 'About Us'
+    the_header = 'About Us!!!'
+    return render(request, "Home/home.html", {'title': the_title, 'header': the_header})
 
 
 def contact_page(request):
-    return HttpResponse("<h1>Contact Us</h1>")
+    the_title = 'Contact Us'
+    the_header = 'Contact Us!!!'
+    return render(request, "Home/home.html", {'title': the_title, 'header': the_header})
 
 
 def features(request):
-    return HttpResponse("<h1>Features</h1>")
+    the_title = 'Features'
+    the_header = 'Features!!!'
+    return render(request, "Home/home.html", {'title': the_title, 'header': the_header})
 
 
 def preview(request):
-    return HttpResponse("<h1>Preview</h1>")
+    the_title = 'Preview'
+    the_header = 'Preview!!!'
+    return render(request, "Home/home.html", {'title': the_title, 'header': the_header})
