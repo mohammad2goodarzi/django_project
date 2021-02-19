71a0193ab47ca858ffc60e6293dd0431e5e13674
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2021-02-19 17:45:21 +0330

    Add url for new views

--- my_site/urls.py
+++ my_site/urls.py
@@ -14,15 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from .views import home_page, about_page, contact_page, features, preview
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page),
-    path('about-us', about_page),
-    path('contact-us', contact_page),
-    path('preview', preview),
-    path('features', features),
+    path('about-us/', about_page),
+    path('contact-us/', contact_page),
+    path('preview/', preview),
+    path('features/', features),
 ]
