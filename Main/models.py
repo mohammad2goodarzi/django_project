bb4722bd1293e82391e3e334a3e438280bb539df
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2021-02-23 08:02:43 +0330

    first model

--- Main/models.py
+++ Main/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 
 class BlogPost(models.Model):
-    post_title = models.TextField()
+    post_title = models.CharField(max_length=200)
     post_content = models.TextField(null=True, blank=True)
+
+
