5225a61332c94d323bd72aff5cb86643ac3918c3
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2021-02-23 22:03:03 +0330

    A unique slug

--- Main/models.py
+++ Main/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class BlogPost(models.Model):
     post_title = models.CharField(max_length=200)
     post_content = models.TextField(null=True, blank=True)
-    slug = models.SlugField(default="")
+    slug = models.SlugField(default="", unique=True)
 
 
--- Main/views.py
+++ Main/views.py
@@ -5,11 +5,11 @@ from .models import BlogPost
 
 
 def blog_post_details(request, slug):
-    # objects = get_object_or_404(BlogPost, slug=slug)
-    objects = None
-    queryset = BlogPost.objects.filter(slug=slug)
-    if queryset.count() >= 1:
-        objects = queryset.first()
+    objects = get_object_or_404(BlogPost, slug=slug)
+    # objects = None
+    # queryset = BlogPost.objects.filter(slug=slug)
+    # if queryset.count() >= 1:
+    #     objects = queryset.first()
     template_name = 'Blog/blog_post.html'
     context = {'object': objects}
     return render(request, template_name, context)
