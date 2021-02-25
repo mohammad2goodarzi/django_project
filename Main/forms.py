aa465e201933121e73d802b31e2ffe6561cd4cee
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2021-02-25 14:44:19 +0330

    clean_title commented

--- Main/forms.py
+++ Main/forms.py
@@ -7,10 +7,13 @@ class BlogPostModelForm(forms.ModelForm):
         model = BlogPost
         fields = ['post_title', 'post_content', 'slug']
 
-    def clean_title(self, *args, **kwargs):
-        title = self.cleaned_data.get('post_title')
-        print(title)
-        qs = BlogPost.oblects.filter(post_title__iexact=title)
-        if qs.exists():
-            raise forms.ValidationError("This title has already been used.")
-        return title
+    # def clean_title(self, *args, **kwargs):
+    #     instance = self.instance
+    #     title = self.cleaned_data.get('post_title')
+    #     qs = BlogPost.objects.filter(post_title__iexact=title)
+    #     print(f'... {title} ...')
+    #     if instance is not None:
+    #         qs = qs.exclude(pk=instance.pk)
+    #     if qs.exists():
+    #         raise forms.ValidationError("This title has already been used.")
+    #     return title
