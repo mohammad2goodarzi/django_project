80d0c67e3dd0a90652b5bc7d412509fbba9f898e
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2021-03-08 00:13:01 +0330

    admin page modified

--- Main/admin.py
+++ Main/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import BlogPost
 
 
+admin.site.site_header = 'The_project Admin'
+admin.site.index_title = 'Main Admin'
+admin.site.site_title = 'django'
+
 admin.site.register(BlogPost)
